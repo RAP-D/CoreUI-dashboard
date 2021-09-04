c88d2e9610e2a11c05e37b52cb57edd3828106c3
Praveen Seneviratne <63094219+Gudnite@users.noreply.github.com>
2021-09-04 15:56:05 +0530

    code clear

--- src/views/dashboard/Diagram/Diagram.js
+++ src/views/dashboard/Diagram/Diagram.js
@@ -14,70 +14,12 @@ const Diagram = ()=>{
              <Xwrapper>   
              <ResizeObserver
                 onResize={useXarrow()}>
-                {/* <div className="bulma-col columns is-2 is-multiline is-mobile">
-                         <div className="column  is-4">
-                                 <center>
-                                 <div className="heightadjust">
-                                         <div className = "nodeIcon" id="Generation">
-                                                 <img className="icon loku" src={SolarPanel} alt="Generation"/>
-                                                 <div className="nodename">Generation</div>
-                                         </div>
-                                 </div>
-                                 </center>
-                         </div>
-                         <div className="column is-4"></div>
-         
-                         <div className="column is-4">
-                                 <center>
-                                 <div className="heightadjust">
-                                         <div className = "nodeIcon" id="Grid">
-                                                 <img className="icon loku" src={PowerTower} alt="Grid"/>
-                                                 <div className="nodename">Grid</div>
-                                         </div>
-                                 </div>
-                                 </center>
-                         </div>
-                         <div className="column is-4"></div>
-         
-                         <div className="column is-4">
-                                 <img className="icon tower" src={Stortera} alt="StorTower" id="StorTower"/>
-                                 <div>StorTower</div>
-                         </div>
-                         <div className="column is-4"></div>
-         
-                         <div className="column is-4">
-                                 <center>
-                                 <div className="heightadjust">
-                                         <div className = "nodeIcon" id="Battery">
-                                                 <img className="icon loku" src={Battery} alt="Battery"/>
-                                                 <div className="nodename">Battery</div>
-                                         </div>
-                                 </div>
-                                 </center>
-                         </div>
-                         <div className="column is-4"></div>
-                         
-                         <div className="column is-4">
-                                 <center>
-                                 <div className="heightadjust">
-                                         <div className = "nodeIcon" id="Home">
-                                                 <img className="icon loku" src={Home} alt="Home"/>
-                                                 <div className="nodename">Home</div>
-                                         </div>
-                                 </div>
-                                 </center>
-                         </div>
-                         <Xarrow start="StorTower" startAnchor= {["right", {position: "left", offset: {y: -20}}]}  end="Generation" endAnchor="bottom" color='#64B42C' path='grid' dashness={{ animation: 1 }}  showHead={false} labels={{start:<div style={{ fontSize: "1.3em", marginRight: "3rem", marginBottom: "2rem",color: "#64B42C"}}>0.00 Kwh</div>}}/>
-                         <Xarrow start="StorTower" startAnchor= {["left", {position: "right", offset: {y: -20}}]} end="Grid" endAnchor="bottom" color='#64B42C' path='grid' dashness={{ animation: 1 }}  showHead={false} labels={{start:<div style={{ fontSize: "1.3em", marginLeft: "3rem", marginBottom: "2rem",color: "#64B42C"}}>0.00 Kwh</div>}}/>
-                         <Xarrow start="StorTower" startAnchor= {["right", {position: "left", offset: {y: 20}}]} end="Battery" endAnchor="top" color='#64B42C' path='grid' dashness={{ animation: 1 }}  showHead={false} labels={{start:<div style={{ fontSize: "1.3em", marginRight: "3rem", marginTop: "5rem",color: "#64B42C"}}>0.00 Kwh</div>}}/>
-                         <Xarrow start="StorTower" startAnchor= {["left", {position: "right", offset: {y: 20}}]} end="Home" endAnchor="top"color='#64B42C' path='grid' dashness={{ animation: 1 }}  showHead={false} labels={{start:<div style={{ fontSize: "1.3em", marginLeft: "3rem", marginTop: "5rem",color: "#64B42C"}}>0.00 Kwh</div>}}/>
-                     </div> */}
                      <CContainer>
                              <CRow className="justify-content-center">
                                         <CCol xs={6}>
                                                 <CContainer className="float-left heightadjust">
                                                         <CContainer className="nodeIcon" id="Generation">
-                                                                <img className="icon loku" src={SolarPanel} alt="Generation"/><br/>
+                                                                <img className="icon resize" src={SolarPanel} alt="Generation"/><br/>
                                                                 <span className="nodeName">Generation</span>
                                                         </CContainer>
                                                 </CContainer>
@@ -88,7 +30,7 @@ const Diagram = ()=>{
                                         <CCol xs={6}>
                                         <CContainer className="float-right heightadjust">
                                                 <CContainer className="nodeIcon" id="Grid">
-                                                                <img className="icon loku" src={PowerTower} alt="Grid"/><br/>
+                                                                <img className="icon resize" src={PowerTower} alt="Grid"/><br/>
                                                                 <span className="nodeName">Grid</span>
                                                         </CContainer>
                                                 </CContainer>
@@ -105,7 +47,7 @@ const Diagram = ()=>{
                                         <CCol xs={6}>
                                         <CContainer className="float-left heightadjust">
                                                         <CContainer className="nodeIcon" id="Battery">
-                                                                <img className="icon loku" src={Battery} alt="Battery"/><br/>
+                                                                <img className="icon resize" src={Battery} alt="Battery"/><br/>
                                                                 <span className="nodeName">Battery</span>
                                                         </CContainer>
                                                 </CContainer>
@@ -116,7 +58,7 @@ const Diagram = ()=>{
                                         <CCol xs={6}>
                                         <CContainer className="float-right heightadjust">
                                                         <CContainer className="nodeIcon" id="Home">
-                                                                <img className="icon loku" src={Home} alt="Home"/><br/>
+                                                                <img className="icon resize" src={Home} alt="Home"/><br/>
                                                                 <span className="nodeName">Home</span>
                                                         </CContainer>
                                                 </CContainer>
