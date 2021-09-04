824ff9917e645932955182024cc78eb47ab30e15
Praveen Seneviratne <63094219+Gudnite@users.noreply.github.com>
2021-09-04 13:25:31 +0530

    coreUI diagram

--- src/views/dashboard/Diagram/Diagram.js
+++ src/views/dashboard/Diagram/Diagram.js
@@ -7,13 +7,14 @@ import Stortera from './stortera.svg';
 import Battery from './battery-status.svg';
 import Home from './lightbulb.svg';
 import ResizeObserver from 'rc-resize-observer';
+import { CContainer, CRow, CCol } from "@coreui/react";
 
 const Diagram = ()=>{
         return(
              <Xwrapper>   
              <ResizeObserver
                 onResize={useXarrow()}>
-                <div className="bulma-col columns is-2 is-multiline is-mobile">
+                {/* <div className="bulma-col columns is-2 is-multiline is-mobile">
                          <div className="column  is-4">
                                  <center>
                                  <div className="heightadjust">
@@ -70,7 +71,66 @@ const Diagram = ()=>{
                          <Xarrow start="StorTower" startAnchor= {["left", {position: "right", offset: {y: -20}}]} end="Grid" endAnchor="bottom" color='#64B42C' path='grid' dashness={{ animation: 1 }}  showHead={false} labels={{start:<div style={{ fontSize: "1.3em", marginLeft: "3rem", marginBottom: "2rem",color: "#64B42C"}}>0.00 Kwh</div>}}/>
                          <Xarrow start="StorTower" startAnchor= {["right", {position: "left", offset: {y: 20}}]} end="Battery" endAnchor="top" color='#64B42C' path='grid' dashness={{ animation: 1 }}  showHead={false} labels={{start:<div style={{ fontSize: "1.3em", marginRight: "3rem", marginTop: "5rem",color: "#64B42C"}}>0.00 Kwh</div>}}/>
                          <Xarrow start="StorTower" startAnchor= {["left", {position: "right", offset: {y: 20}}]} end="Home" endAnchor="top"color='#64B42C' path='grid' dashness={{ animation: 1 }}  showHead={false} labels={{start:<div style={{ fontSize: "1.3em", marginLeft: "3rem", marginTop: "5rem",color: "#64B42C"}}>0.00 Kwh</div>}}/>
-                     </div>
+                     </div> */}
+                     <CContainer>
+                             <CRow className="justify-content-center">
+                                        <CCol xs={4}>
+                                                <CContainer className="heightadjust">
+                                                        <CContainer className="nodeIcon" id="Generation">
+                                                                <img className="icon loku" src={SolarPanel} alt="Generation"/><br/>
+                                                                <span className="nodeName">Generation</span>
+                                                        </CContainer>
+                                                </CContainer>
+                                        </CCol>
+
+                                        <CCol xs={4}></CCol>
+
+                                        <CCol xs={4}>
+                                        <CContainer className="heightadjust">
+                                                <CContainer className="nodeIcon" id="Grid">
+                                                                <img className="icon loku" src={PowerTower} alt="Grid"/><br/>
+                                                                <span className="nodeName">Grid</span>
+                                                        </CContainer>
+                                                </CContainer>
+                                        </CCol>
+                                        
+                             </CRow>
+                             <CRow className="justify-content-center">
+
+                                        <CCol xs={4}></CCol>
+
+                                        <CCol xs={4}>
+                                                <img className="icon tower" src={Stortera} alt="StorTower" id="StorTower"/><br/>
+                                                <span className="nodeName">StorTower</span>
+                                        </CCol>
+                                        <CCol xs={4}></CCol>
+                             </CRow>
+                             <CRow className="justify-content-center">
+                                        <CCol xs={4}>
+                                        <CContainer className="heightadjust">
+                                                        <CContainer className="nodeIcon" id="Battery">
+                                                                <img className="icon loku" src={Battery} alt="Battery"/><br/>
+                                                                <span className="nodeName">Battery</span>
+                                                        </CContainer>
+                                                </CContainer>
+                                        </CCol>
+
+                                        <CCol xs={4}></CCol>
+
+                                        <CCol xs={4}>
+                                        <CContainer className="heightadjust">
+                                                        <CContainer className="nodeIcon" id="Home">
+                                                                <img className="icon loku" src={Home} alt="Home"/><br/>
+                                                                <span className="nodeName">Home</span>
+                                                        </CContainer>
+                                                </CContainer>
+                                        </CCol>
+                             </CRow>
+                                <Xarrow start="StorTower" startAnchor= {["right", {position: "left", offset: {y: -20}}]}  end="Generation" endAnchor="bottom" color='#64B42C' path='grid' dashness={{ animation: 1 }}  showHead={false} labels={{start:<div style={{ fontSize: "1.3em", marginRight: "3rem", marginBottom: "2rem",color: "#64B42C"}}>0.00 Kwh</div>}}/>
+                                <Xarrow start="StorTower" startAnchor= {["left", {position: "right", offset: {y: -20}}]} end="Grid" endAnchor="bottom" color='#64B42C' path='grid' dashness={{ animation: 1 }}  showHead={false} labels={{start:<div style={{ fontSize: "1.3em", marginLeft: "3rem", marginBottom: "2rem",color: "#64B42C"}}>0.00 Kwh</div>}}/>
+                                <Xarrow start="StorTower" startAnchor= {["right", {position: "left", offset: {y: 20}}]} end="Battery" endAnchor="top" color='#64B42C' path='grid' dashness={{ animation: 1 }}  showHead={false} labels={{start:<div style={{ fontSize: "1.3em", marginRight: "3rem", marginTop: "5rem",color: "#64B42C"}}>0.00 Kwh</div>}}/>
+                                <Xarrow start="StorTower" startAnchor= {["left", {position: "right", offset: {y: 20}}]} end="Home" endAnchor="top"color='#64B42C' path='grid' dashness={{ animation: 1 }}  showHead={false} labels={{start:<div style={{ fontSize: "1.3em", marginLeft: "3rem", marginTop: "5rem",color: "#64B42C"}}>0.00 Kwh</div>}}/>
+                     </CContainer>
                 </ResizeObserver>
             </Xwrapper>
         )
