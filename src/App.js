92aef08631f60e10c4877bccb196ce7427905276
Dasith Rathnasinghe <dasithrathnasinghe@gmail.com>
2021-10-29 00:52:43 +0530

    minor ui changers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { HashRouter, Route, Switch} from 'react-router-dom';
 import './scss/style.scss';
+import { useSelector } from 'react-redux';
 
 const loading = (
   <div className="pt-3 text-center">
@@ -13,26 +14,17 @@ const TheLayout = React.lazy(() => import('./containers/TheLayout'));
 
 // Pages
 const Login = React.lazy(() => import('./views/pages/login/Login'));
-const Register = React.lazy(() => import('./views/pages/register/Register'));
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
-
-class App extends Component {
-  render() {
+const App =()=> {
     return (
       <HashRouter>
           <React.Suspense fallback={loading}>
             <Switch>
-              <Route exact path="/" name="Login Page" render={props => <Login {...props}/>} />
-              <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
-              <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
-              <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
-              <Route path="/home" name="Home" render={props => <TheLayout {...props}/>} />
+              <Route exact path="/" name="Login Page" render={props => <Login {...props}/>}/>
+              {useSelector(state=>state.user)==='admin'?<Route path="/home" name="Home" render={props => <TheLayout {...props}/>} />:null}
             </Switch>
           </React.Suspense>
       </HashRouter>
     );
-  }
 }
 
 export default App;
